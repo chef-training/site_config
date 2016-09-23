7f2398c6bb6b5fa91cf8055bec8c6d4d0e86b383
Franklin Webber <franklin.webber@gmail.com>
2016-09-23 12:54:27 -0500

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'site_config'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures site_config'
+long_description 'Installs/Configures site_config'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/site_config/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/site_config' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: site_config
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
