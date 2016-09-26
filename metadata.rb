dd0c32bf6b2b4b80bf3dd0badf6df57e52ca230d
Franklin Webber <franklin.webber@gmail.com>
2016-09-26 11:24:13 -0500

    Fixes the metadata to include missing urls

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,5 @@ description 'Installs/Configures site_config'
 long_description 'Installs/Configures site_config'
 version '0.1.0'
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/site_config/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/site_config' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-training/site_config/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-training/site_config' if respond_to?(:source_url)
