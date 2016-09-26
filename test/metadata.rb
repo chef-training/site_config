48aec61e7987597c508dd9a99a12b17ac2ef07b8
Franklin Webber <franklin.webber@gmail.com>
2016-09-26 11:26:21 -0500

    This is a workaround to a Chef DK issue (https://github.com/chef/chef-dk/issues/1027)

---
+++ test/metadata.rb
@@ -0,0 +1,10 @@
+name 'site_config'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures site_config'
+long_description 'Installs/Configures site_config'
+version '0.1.0'
+
+issues_url 'https://github.com/chef-training/site_config/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-training/site_config' if respond_to?(:source_url)
